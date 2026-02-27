3b5add4f8b633ba3fc6bdbe0079c6d6180794401
Dipto Rahman <dipto2313@gmail.com>
2026-02-27 13:05:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1464739831-da270",
+  "appId": "1:56418023011:web:b4029b54b7ab01c5a72a56",
+  "apiKey": "AIzaSyD5SeYOMzsMrXG3qJhqcPTgunjIhZfhUhc",
+  "authDomain": "studio-1464739831-da270.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "56418023011"
+};
